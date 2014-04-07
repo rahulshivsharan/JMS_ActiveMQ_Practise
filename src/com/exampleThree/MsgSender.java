e53eb09eadc76f10ae14216cd8fd49c6b853ee43
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-04-07 18:14:09 +0530

    Create MsgSender.java

---
+++ src/com/exampleThree/MsgSender.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.exampleThree;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+public class MsgSender {
+    private static void sendMsg() throws Exception{
+        final String url = "tcp://localhost:61616";
+        final ConnectionFactory cf = new ActiveMQConnectionFactory(url);
+        Connection con = cf.createConnection();
+        con.start();
+        
+        final Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        
+        final Destination destination = session.createQueue("MYQUEUE");
+        
+        final MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        DateFormat formate = new SimpleDateFormat("d MMM yyyy h:mm:ss a");
+
+        for(int i=1; i <= 20; i++){
+            TextMessage msg = session.createTextMessage();
+            if(i < 20){
+                msg.setText(i+" MESSAGE : "+formate.format(new Date()));
+            }else{
+                msg.setText("done");
+            }
+            
+            producer.send(msg);
+            Thread.sleep(2000);
+        }        
+        session.close();
+        con.close();
+    }
+    
+    public static void main(String [] args){        
+        try{
+            sendMsg();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
